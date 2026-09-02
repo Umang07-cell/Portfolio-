5fa5c89079a0621bcdacc7a39e8cacce54bba4e8
umang pawar <umangpawar629@gmail.com>
2026-09-02 21:44:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Navbar from "./components/Navbar";
 import HeroV2 from "./components/HeroV2";
 import Intro from "./components/Intro";
+import Work from "./components/Work";
 import ProjectsSlider from "./components/ProjectsSlider";
+import TechStack from "./components/TechStack";
+import Experience from "./components/Experience";
 import TimelineV2 from "./components/TimelineV2";
 import FooterV2 from "./components/FooterV2";
 import SmoothScroll from "./components/SmoothScroll";
@@ -13,7 +16,10 @@ function App() {
       <main>
         <HeroV2 />
         <Intro />
+        <Work />
         <ProjectsSlider />
+        <TechStack />
+        <Experience />
         <TimelineV2 />
       </main>
       <FooterV2 />
